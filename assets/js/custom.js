d186d58d836f1af26d050ea4ae90efc33e75f539
numselli <58607232+numselli@users.noreply.github.com>
2020-12-30 17:29:01 -0800

    improve js for photo galery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,7 @@
+$( document ).ready(pageLoad);
+$(window).bind('hashchange', pageLoad);
+document.getElementById('contactForm').addEventListener('submit', sendSubmit)
+document.getElementById('contactForm').addEventListener('reset', reset)
 
 // contact
 function sendSubmit(event) {
@@ -25,6 +29,7 @@ function sendSubmit(event) {
 		fetch('https://discordapp.com/api/webhooks/779986049207762945/AOBq-yiXCk64dOg5G7V3Fp8a98fl4jidGB-kyz5r9s9JqE-kp1VpepkrNav6q2MO6Nuw',{method: 'post',headers: {'Content-Type': 'application/json',},body: JSON.stringify({embeds: [{color: 11730954,title: 'New Message!',timestamp: new Date(),fields: [{"name": "Name","value": document.contactForm.name.value,"inline": true},{"name": "Email","value": document.contactForm.email.value,"inline": true},{"name": "Message","value": document.contactForm.message.value}]}]})})
 		.then(response => {
 			if (response.ok){
+				reset()
 				console.log("Success");
 			}
 		})
@@ -32,35 +37,26 @@ function sendSubmit(event) {
 	}
 }
 function reset(){
+	document.contactForm.reset()
 	document.contactForm.name.style = "", document.contactForm.email.style = "", document.contactForm.message.style = "";
 }
-document.getElementById('contactForm').addEventListener('submit', sendSubmit)
-document.getElementById('contactForm').addEventListener('reset', reset)
 
-
-// photogallery
-const targetNode = document.getElementById('photogallery');
-const config = { classes: true, attributes: true, childList: true, subtree: true };
-const callback = async function (mutationsList, observer) {
-	for (const mutation of mutationsList) {
-		if (mutation.attributeName === "class") {
-			if (mutation.target.className === "active") {
-				const response = await fetch('https://api.github.com/repos/numselli/numselli.github.io/contents/photogallery_images/');
-				const data = await response.json();
-				for (let file of data) {
-					$('#photos').append(`<div id="${file.name}"><h3 class="major">${file.name}</h3> <ul id="${file.name}ImgContainer" class="ul"></div>`)
-					$.ajax({
-						url: `https://api.github.com/repos/numselli/numselli.github.io/contents/${file.path}`,
-						success: function (data) {
-							data.map(({ download_url, name }) => {
-								$(`#${file.name.replaceAll('%20', "")}ImgContainer`).append(`<li class="li"><img src="${download_url}" alt="${name}" loading="lazy"></li>`)
-							});
-						}
-					});
-				}
-			} 
-		}
+async function pageLoad(){
+	switch((window.location.hash).replace("#","")){
+		case `photogallery`:
+			const response = await fetch('https://api.github.com/repos/numselli/numselli.github.io/contents/photogallery_images/');
+			const data = await response.json();
+			for (let file of data) {
+				$('#photos').append(`<div id="${file.name}"><h3 class="major">${file.name}</h3> <ul id="${file.name}ImgContainer" class="ul"></div>`)
+				$.ajax({
+					url: `https://api.github.com/repos/numselli/numselli.github.io/contents/${file.path}`,
+					success: function (data) {
+						data.map(({ download_url, name }) => {
+							$(`#${file.name.replaceAll('%20', "")}ImgContainer`).append(`<li class="li"><img src="${download_url}" alt="${name}" loading="lazy"></li>`)
+						});
+					}
+				});
+			}
+		break;
 	}
-};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+}

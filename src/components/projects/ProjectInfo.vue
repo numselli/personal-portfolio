926bb5b6000af7aed75340363f7fb85a1e85f379
numselli <58607232+numselli@users.noreply.github.com>
2022-07-19 20:14:23 -0700

    start adding per project info

--- src/components/projects/ProjectInfo.vue
+++ src/components/projects/ProjectInfo.vue
@@ -17,48 +17,6 @@ export default {
 	<div class="block sm:flex gap-0 sm:gap-10 mt-14">
 		<!-- Single project left section details -->
 		<div class="w-full sm:w-1/3 text-left">
-			<!-- Single project client details -->
-			<div class="mb-7">
-				<p
-					class="font-general-medium text-2xl text-secondary-dark dark:text-secondary-light mb-2"
-				>
-					{{ projectInfo.clientHeading }}
-				</p>
-				<ul class="leading-loose">
-					<li
-						v-for="info in projectInfo.companyInfos"
-						:key="info"
-						class="font-general-regular text-ternary-dark dark:text-ternary-light"
-					>
-						<span>{{ info.title }}: </span>
-						<a
-							href="#"
-							:class="
-								info.title == 'Website' || info.title == 'Phone'
-									? 'hover:underline cursor-pointer'
-									: ''
-							"
-							aria-label="Project Website and Phone"
-							>{{ info.details }}</a
-						>
-					</li>
-				</ul>
-			</div>
-
-			<!-- Single project objectives -->
-			<div class="mb-7">
-				<p
-					class="font-general-medium text-2xl text-ternary-dark dark:text-ternary-light mb-2"
-				>
-					{{ projectInfo.objectivesHeading }}
-				</p>
-				<p
-					class="font-general-regular text-primary-dark dark:text-ternary-light"
-				>
-					{{ projectInfo.objectivesDetails }}
-				</p>
-			</div>
-
 			<!-- Single project technologies -->
 			<div class="mb-7">
 				<p
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,55 +1,572 @@
-// Projects data.
-// Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
-
 const projects = [
 	{
 		id: 1,
 		title: 'Counting',
 		category: 'Discord Bot',
 		img: require('@/assets/images/projects/counting/icon.png'),
+		extededDescription:{
+			date: 1579314555000,
+			description: [
+				{
+					id: 1,
+					details: "Discord Chainmail is meant to inform people about fake chain messages on the chat platform Discord. This website was inspired by seeing one of these chain messages and being so frustrated that people believe them. Instead of addressing these messages individually, I decided to create this site to address them all at once. This site primarily focuses on examples from the Discord platform but lots of the points made can apply to other platforms.",
+				}
+			],
+			status: "Active",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Node.js",
+				"JavaScript",
+				"Eris fleet",
+				"Eris",
+				"PostgreSQL",
+				"Vue.js",
+				"Cloudflare pages"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 2,
 		title: 'Kohana',
 		category: 'Discord Bot',
 		img: require('@/assets/images/projects/kohana/icon.png'),
+		extededDescription:{
+			date: 1613510955000,
+			description: [
+				{
+					id: 1,
+					details: "An anime themed multipurpose Discord bot.",
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Node.js",
+				"JavaScript",
+				"Eris",
+				"PostgreSQL"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 3,
 		title: 'Reddit GIF fix',
 		category: 'Discord Bot',
 		img: require('@/assets/images/projects/redditGIFfix/icon.png'),
+		extededDescription:{
+			date: 1649282824000,
+			description: [
+				{
+					id: 1,
+					details: "A Discord bot to properly embed Reddit GIFs.",
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Node.js",
+				"JavaScript",
+				"Eris"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 4,
 		title: 'loading',
 		category: 'Video Game',
 		img: require('@/assets/images/projects/loading/icon.png'),
+		extededDescription:{
+			date: 1564691355000,
+			description: [
+				{
+					id: 1,
+					details: "Will the game ever load? We may never know. While you wait to find out enjoy this simple yet fun game about a loading bar.",
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Unity",
+				"C#"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 5,
 		title: 'Platformer',
 		category: 'Video Game',
 		img: require('@/assets/images/projects/platformer/icon.png'),
+		extededDescription:{
+			date: 1441916955000,
+			description: [
+				{
+					id: 1,
+					details: "Discord Chainmail is meant to inform people about fake chain messages on the chat platform Discord. This website was inspired by seeing one of these chain messages and being so frustrated that people believe them. Instead of addressing these messages individually, I decided to create this site to address them all at once. This site primarily focuses on examples from the Discord platform but lots of the points made can apply to other platforms.",
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Unity",
+				"C#"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 6,
 		title: 'Meme Shooter',
 		category: 'Video Game',
 		img: require('@/assets/images/projects/memeShooter/icon.png'),
+		extededDescription:{
+			date: 1442422986000,
+			description: [
+				{
+					id: 1,
+					details: "Discord Chainmail is meant to inform people about fake chain messages on the chat platform Discord. This website was inspired by seeing one of these chain messages and being so frustrated that people believe them. Instead of addressing these messages individually, I decided to create this site to address them all at once. This site primarily focuses on examples from the Discord platform but lots of the points made can apply to other platforms.",
+				}
+			],
+			status: "Canceled",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"Unity",
+				"C#"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 7,
 		title: 'XD Hacks',
 		category: 'Web Application',
 		img: require('@/assets/images/projects/xdhacks/icon.png'),
+		extededDescription:{
+			date: 1617746824000,
+			description: [
+				{
+					id: 1,
+					details: "XdHacks Mini is a global high-school Hackathon organization dedicated towards hosting high-school Hackathons to inspire more youth to be engaged with the field of STEAM. Our events are where individuals come together and use technology to transform their ideas into reality in an attempt to create a feasible solution towards solving a relevant, real-world problem.",
+				},
+				{
+					id: 2, 
+					details: "Climate Code 2021 is a virtual Hackathon that encourages high-school students from around the world to try and develop prototypes that will help the Environment. Within the span of 36 hours, teams will work together to develop digital ideas aimed towards helping confront environmental challenges that societies encounter today. "
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				'HTML',
+				'CSS',
+				'JavaScript',
+				'Cloudflare pages'
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
 	},
 	{
 		id: 8,
 		title: 'Discord Chainmail',
 		category: 'Web Application',
 		img: require('@/assets/images/projects/chainmail/icon.png'),
-	},
+		extededDescription:{
+			date: 1647916824000,
+			description: [
+				{
+					id: 1,
+					details: "Discord Chainmail is meant to inform people about fake chain messages on the chat platform Discord. This website was inspired by seeing one of these chain messages and being so frustrated that people believe them. Instead of addressing these messages individually, I decided to create this site to address them all at once. This site primarily focuses on examples from the Discord platform but lots of the points made can apply to other platforms.",
+				}
+			],
+			status: "Completed",
+			images: [
+				{
+					id: 1,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/ui-project-1.jpg'),
+				},
+				{
+					id: 2,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/web-project-2.jpg'),
+				},
+				{
+					id: 3,
+					title: 'Kabul Project Management UI',
+					img: require('@/assets/images/mobile-project-2.jpg'),
+				}
+			],
+			tools: [
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"Cloudflare pages"
+			],
+			links: [
+				{
+					id: 1,
+					name: 'Twitter',
+					icon: 'twitter',
+					url: 'https://twitter.com/realstoman',
+				},
+				{
+					id: 2,
+					name: 'Instagram',
+					icon: 'instagram',
+					url: 'https://instagram.com/realstoman',
+				},
+				{
+					id: 3,
+					name: 'Facebook',
+					icon: 'facebook',
+					url: 'https://facebook.com/',
+				},
+				{
+					id: 4,
+					name: 'LinkedIn',
+					icon: 'linkedin',
+					url: 'https://linkedin.com/',
+				},
+				{
+					id: 5,
+					name: 'Youtube',
+					icon: 'youtube',
+					url: 'https://www.youtube.com/c/StomanStudio',
+				}
+			]
+		}
+	}
 ];
 
 export default projects;
